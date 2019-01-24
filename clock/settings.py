7979c83ed8b336abc99b724ef1ba95b45c8e12e8
Janeen Scott <janeen.e.scott@gmail.com>
2019-01-24 14:59:01 -0500

    fized middleware error

--- clock/settings.py
+++ clock/settings.py
@@ -66,8 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    ‘whitenoise.middleware.WhiteNoiseMiddleware',
-
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'clock.urls'
